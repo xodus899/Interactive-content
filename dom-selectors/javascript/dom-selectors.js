e663866a2105a728bc918a0c8c8066399154e3ef
Christopher Justice <justcd@mail.broward.edu>
2017-11-05 11:26:27 -0500

    Query selectors

---
+++ dom-selectors/javascript/dom-selectors.js
@@ -0,0 +1,21 @@
+// Old JS
+
+// var tag = document.getElementById("highlight");
+// var classTags = document.getElementsByClassName("bolded");
+// var elementTags = document.getElementsByTagName("li");
+// var firstP = document.getElementById("first");
+
+
+
+//  Es2015
+// querySelector returns the first match: querySelectorAll returns all matches
+
+// let tag = document.querySelector("#highlight");
+// let classTags = document.querySelectorAll(".bolded");
+// let elementTags = document.querySelectorAll("li");
+// let firstP = document.querySelector("#first");
+// let firstP = document.querySelectorAll("#first");
+// let firstP = document.querySelector("p#first");
+
+
+
