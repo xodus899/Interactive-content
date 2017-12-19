bb9c16e64eec36fe7913f8a2086e053ee1f018b8
Christopher Justice <justcd@mail.broward.edu>
2017-12-19 13:42:48 -0500

    Classes

--- j-methods/javascript/j-methods.js
+++ j-methods/javascript/j-methods.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $("img").css("width", "300px");
+$("img").css("height", "200px");
 
 $("img").attr("src", "https://c1.staticflickr.com/9/8339/28469527266_c9c04724e9_b.jpg");
 
@@ -9,3 +10,12 @@ $("img:first-of-type").attr("src", "https://www.forestholidays.co.uk/~/media/ima
 $("img:last-of-type").attr("src", "https://www.forestholidays.co.uk/~/media/images/content%20block%20images/slideshow%20images/themes/wildlife/pine-marten-closeup.ashx?la=en ");
 
 $("input").val();
+
+$("h1").addClass("correct");
+$("h1").removeClass("correct");
+
+$("li").addClass("wrong");
+$("li").css("width", "50px");
+
+
+$("li").first().toggleClass("done");
