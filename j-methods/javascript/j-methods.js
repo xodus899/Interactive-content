febaf814dab8c6a8d2fdeb4a28baecf55cd9f10f
Christopher Justice <justcd@mail.broward.edu>
2017-12-19 13:11:23 -0500

    Use attr

---
+++ j-methods/javascript/j-methods.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$("img").css("width", "300px");
+
+$("img").attr("src", "https://c1.staticflickr.com/9/8339/28469527266_c9c04724e9_b.jpg");
+
+$("img:first-of-type").attr("src", "https://www.forestholidays.co.uk/~/media/images/content%20block%20images/slideshow%20images/themes/wildlife/pine-marten-closeup.ashx?la=en ");
+
+$("img:last-of-type").attr("src", "https://www.forestholidays.co.uk/~/media/images/content%20block%20images/slideshow%20images/themes/wildlife/pine-marten-closeup.ashx?la=en ");
